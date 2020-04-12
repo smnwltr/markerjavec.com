19590886a8c7402c7cee21e4b3fcacd5d8e5b50c
smnwltr <smn@wltr.co>
2020-04-12 13:00:32 -0600

    Update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,20 @@
 
+// Add white background to navbar upon scroll
+
+$(document).ready(function () {
+    //caches a jQuery object containing the header element
+    var header = $(".navbar");
+    $(window).scroll(function () {
+        var scroll = $(window).scrollTop();
+
+        if (scroll >= 250) {
+            header.addClass("navbar-scroll");
+        } else {
+            header.removeClass("navbar-scroll")
+        }
+    });
+});
+
 // Insert year for copyright
 document.getElementById("currentYear").innerHTML = new Date().getFullYear();
 
